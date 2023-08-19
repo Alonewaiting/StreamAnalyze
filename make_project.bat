825f0b70f113c0a638e5f9746ea8fcb650117b9d
dengwenyao <272987067@qq.com>
2023-08-19 10:40:12 +0800

    change qt path build.

--- make_project.bat
+++ make_project.bat
@@ -1,1 +1,3 @@
-cmake -S . -B sln_x64 -G "Visual Studio 16 2019" -A x64  -DCMAKE_BUILD_TYPE=Debug -DBUILD_TARGET=windows 
+set "QtPath6=D:\Qt\6.4.3\msvc2019_64"
+set /p "QtPath6=Enter path or just ENTER for default [%QtPath6%] :
+cmake -S . -B sln_x64 -G "Visual Studio 16 2019" -A x64  -DCMAKE_BUILD_TYPE=Debug -DBUILD_TARGET=windows -DQT_DIR:STRING=${QtPath6}
